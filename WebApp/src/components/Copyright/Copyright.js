dac7b331a14c232b0abaecb611e4ea1da6155719
hachchi <161139489+hachchi@users.noreply.github.com>
2024-03-04 21:46:28 +1100

    docs: update Copyright.js comments

--- WebApp/src/components/Copyright/Copyright.js
+++ WebApp/src/components/Copyright/Copyright.js
@@ -1,30 +1,37 @@
+// Importing necessary components from Material-UI library
 import React from "react";
 import { Link as MaterialLink, Typography, Box } from "@material-ui/core";
 
+// Functional component for displaying copyright information
 const Copyright = () => {
+  // Styling for the container div
   const containerStyle = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#014421",
-    color: "white",
-    padding: 20,
-    width: "100%",
-    height: "100%",
+    backgroundColor: "#014421", // Background color
+    color: "white", // Text color
+    padding: 20, // Padding around the content
+    width: "100%", // Full width
+    height: "100%", // Full height
   };
 
+  // Styling for the Typography component
   const typographyStyle = {
-    color: "white",
-    fontFamily: "Montserrat-Regular",
+    color: "white", // Text color
+    fontFamily: "Montserrat-Regular", // Font family
   };
 
+  // Render the copyright information using Material-UI components
   return (
     <Box style={containerStyle}>
       <Typography variant="h6" color="textSecondary" align="center" style={typographyStyle}>
+        {/* Copyright text with a link to www.SCoReLab.org */}
         {"Copyright © "}
         <MaterialLink color="inherit" href="https://www.scorelab.org">
           www.SCoReLab.org
         </MaterialLink>{" "}
+        {/* Display the current year dynamically */}
         {new Date().getFullYear()}
         {"."}
       </Typography>
@@ -32,4 +39,5 @@ const Copyright = () => {
   );
 };
 
+// Export the Copyright component for use in other parts of the application
 export default Copyright;
