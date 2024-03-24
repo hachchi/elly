4e41da3be7a1056d923bbb2895646adfb76e0137
hachchi <161139489+hachchi@users.noreply.github.com>
2024-03-24 22:54:20 +1100

    docs: update SmallCard.js

--- WebApp/src/components/SmallCard/SmallCard.js
+++ WebApp/src/components/SmallCard/SmallCard.js
@@ -3,8 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import { blue } from "@material-ui/core/colors";
-// import Icon from "@material-ui/core/Icon";
 import { Typography } from "@material-ui/core";
+
 const useStyles = makeStyles((theme) => ({
   card: {
     margin: 0,
@@ -16,6 +16,14 @@ const useStyles = makeStyles((theme) => ({
   avatar: {
     backgroundColor: blue[500],
   },
+  title: {
+    fontFamily: "Montserrat-Regular",
+    fontSize: theme.typography.h5.fontSize,
+  },
+  count: {
+    fontFamily: "Montserrat-Regular",
+    fontSize: theme.typography.h3.fontSize,
+  },
 }));
 
 export default function SmallCard(props) {
@@ -24,30 +32,16 @@ export default function SmallCard(props) {
   return (
     <Card className={classes.card}>
       <CardHeader
-        // avatar={() => (
-        //   <Icon style={{ fontSize: 120, color: "white" }} color="action">
-        //     {props.icon}
-        //   </Icon>
-        // )}
         title={
-          <Typography
-            style={{ fontFamily: "Montserrat-Regular" }}
-            component="h2"
-            variant="h5"
-          >
+          <Typography className={classes.title} component="h2" variant="h5">
             {props.type}
           </Typography>
         }
         subheader={
-          <Typography
-            style={{ fontFamily: "Montserrat-Regular" }}
-            component="h2"
-            variant="h3"
-          >
+          <Typography className={classes.count} component="h2" variant="h3">
             {props.count}
           </Typography>
         }
-        style={{ fontSize: 50 }}
       />
     </Card>
   );
