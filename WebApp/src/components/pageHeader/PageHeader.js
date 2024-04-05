935225ee3c19f2e4bc45a505b46ab369a655acdf
hachchi <161139489+hachchi@users.noreply.github.com>
2024-04-06 02:40:28 +1100

    docs: update PageHeader.js

--- WebApp/src/components/pageHeader/PageHeader.js
+++ WebApp/src/components/pageHeader/PageHeader.js
@@ -3,22 +3,27 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
+// Define styles using makeStyles hook
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3, 2),
+    padding: theme.spacing(3, 2), // Padding applied to the root element
   },
 }));
 
+// Functional component PageHeader
 export default function PageHeader(props) {
-  const classes = useStyles();
+  const classes = useStyles(); // Apply styles using useStyles hook
 
   return (
+    // Paper component with custom styles
     <Paper className={classes.root}>
+      {/* Typography component for the title */}
       <Typography variant="h5" component="h3">
-        {props.title}
+        {props.title} {/* Render title passed as props */}
       </Typography>
+      {/* Typography component for the subtitle */}
       <Typography component="p">
-        {props.subtitle}
+        {props.subtitle} {/* Render subtitle passed as props */}
       </Typography>
     </Paper>
   );
